51e49d570b92454e5c404e5267d7fcc182e49b88
widgetinfo <drastik.widget@gmail.com>
2024-07-08 10:08:41 +0530

    first change

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {x
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
